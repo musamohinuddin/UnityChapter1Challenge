44f0963a09b6fed1a4a12c662836797436245739
K S M Musa Mohinuddin <musamohinuddin@gmail.com>
2021-03-14 23:44:58 +0530

    Updated Scene Navigation for 3 scenes with Back Button.

---
+++ Assets/Scripts/SceneNavigation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneNavigation : MonoBehaviour
+{
+    public Button button;
+	public string newScene;
+	
+	// Start is called before the first frame update
+    void Start()
+    {
+        button.onClick.AddListener(OnButtonClick); 
+    }
+	
+	public void OnButtonClick()
+	{
+		Debug.Log("Button Clicked");
+		SceneManager.LoadScene(newScene);
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
